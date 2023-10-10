6ba486bdf5c670e1529955243e7b3b6774ce79a9
DSP Chathuranga <dspchathuranga@gmail.com>
2023-10-10 10:42:24 +0100

    View Todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,7 +5,13 @@ function Todos() {
     const todos = useSelector(state => state.todos);
     console.log(todos)
   return (
-    <div>Todos</div>
+    <>
+        <div>Todos</div>
+        {todos.map((todo) => (
+            <div key={todo.id}>{todo.text}</div>
+        ))}
+    </>
+    
   )
 }
 
